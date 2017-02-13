5bde9d9312f0b0d7e16a6679b402c762fe6ce1b9
Trym Haakon Blaauw <thblaauw@stud.ntnu.no>
2017-02-13 14:24:38 +0100

    Made Room-class

---
+++ src/RooME/LectureHall.java
@@ -0,0 +1,31 @@
+package RooME;
+
+public class Room{
+
+	private int space;
+	private boolean projector;
+	private boolean experimentable;
+	
+	
+	public int getSpace() {
+		return space;
+	}
+	public void setSpace(int space) {
+		this.space = space;
+	}
+	public boolean isProjector() {
+		return projector;
+	}
+	public void setProjector(boolean projector) {
+		this.projector = projector;
+	}
+	public boolean isExperimentable() {
+		return experimentable;
+	}
+	public void setExperimentable(boolean experimentable) {
+		this.experimentable = experimentable;
+	}
+	
+	
+
+}
