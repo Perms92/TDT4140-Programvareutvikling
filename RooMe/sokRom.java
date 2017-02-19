f790a1c2b1fdb5a26dc9c608e45b3416e96fe7aa
Kristian Langvann <Creztian@Kristians-MacBook-Pro.local>
2017-02-19 14:27:56 +0100

    no message

---
+++ RooMe/sokRom.java
@@ -0,0 +1,12 @@
+package RooMe;
+
+public class sokRom {
+
+	
+	public static void findRooms(String name, int space, boolean projector, boolean experimentable, boolean blackboard, boolean whiteboard){
+		for (int i = 0; i < romliste.size(); i++) {
+			 System.out.println(romliste.get(i));
+			 //if (romliste.get(i).getSpace() )
+		}
+	}
+}
