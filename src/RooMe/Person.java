a90725e7d7ac183425ec54e17672430c615351c2
Perms92 <perms92@gmail.com>
2017-02-20 13:23:28 +0100

    Made a person from Main

--- src/RooMe/Person.java
+++ src/RooMe/Person.java
@@ -2,23 +2,36 @@ package RooMe;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Person {
 
-	private String firstName, lastName;
+	private String firstName, lastName, proffesion;
 	private int personId;
 	static List<Integer> personIdList = new ArrayList<Integer>();
 	
-	public Person(String firstName, String lastName) {
+	public Person() {
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Enter your first name: ");
+		String firstName = reader.nextLine();
+		System.out.println(firstName);
+		System.out.println("Enter your last name: ");
+		String lastName = reader.nextLine();
+		System.out.println(lastName);
+		System.out.println("Enter your proffesion: ");
+		String proffesion = reader.nextLine();
+		System.out.println(proffesion);
 		this.firstName = firstName;
 		this.lastName = lastName;
+		this.proffesion = proffesion;
 		this.personId = compareId();
+		reader.close();
 	}
 
 	public int compareId() {
 		int x = (int) (Math.random() * 100000 + 1); // Tilfeldig tall i [1, ..., 100000]
 		while (personIdList.contains(x)){
-			x = (int) (Math.random() * 100000 + 1); // Tilfeldig tall i [1, ..., 100000]
+			x = (int) (Math.random() * 900000 + 1); // Tilfeldig tall i [1, ..., 100000]
 		}
 		personIdList.add(x);
 		return x;
@@ -29,18 +42,23 @@ public class Person {
 		return firstName + " " + lastName;
 	}
 
+	public String getProffesion() {
+		return proffesion;
+	}
+	
 	public int getPersonId() {
 		return personId;
 	}
 
 	public String toString() {
-		return "Name: " + getName() + "\nPerson ID: " + personId;
+		return "Name: " + getName() + "\nProffesion: " 
+	+ getProffesion() + "\nPerson ID: " + personId;
 	}
 
 	public static void main(String[] args) {
-		Person Per = new Person("Per Morten", "Solheim");
+		Person Per = new Person();
 		System.out.println(Per.toString());
-		Person Ola = new Person("Ola", "Nordman");
+		Person Ola = new Person();
 		System.out.println(Ola.toString());
 		System.out.println(personIdList);
 	}
