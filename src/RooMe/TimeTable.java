96d99f6adf3479ae514a4c6ea65bd4234e73e716
Perms92 <perms92@gmail.com>
2017-03-27 13:30:32 +0200

    f*** shit

--- src/RooMe/TimeTable.java
+++ src/RooMe/TimeTable.java
@@ -1,20 +1,70 @@
 package RooMe;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.ArrayList;
 
 public class TimeTable {
 
-	List<String> dager = Arrays.asList("mandag", "tirsdag", "onsdag", "torsdag", "fredag");
-	List<Integer> tider = new ArrayList<>(Arrays.asList(1300));
+	private static ArrayList<Object> times = new ArrayList<>();
+	private static ArrayList<Object> days = new ArrayList<>();
 	
-	public TimeTable(){
+	private Cell[][] cells;
+	
+	public TimeTable(String time, String day){
+		/*
+		
+		cells = new Cell[time][day];
+		for (int i = 0; i < time.length(); i++) {
+			String lectureTime = times[i];
+			System.out.println(lectureTime);
+			for (int j = 0; j < day.length(); j++) {
+				String lectureDay = days[j];
+				System.out.println(lectureDay);
+				cells[i][j] = new Cell
+			}
+		}*/
+	}
+	
+	//different subjects with the same professor cannot be active at the same time
+	//timetable should be monday - friday, 08:15 - 20:00
+	
+	public static ArrayList daysList() {
+		days.add("Monday");
+		days.add("Tuesday");
+		days.add("Wednesday");
+		days.add("Thursday");
+		days.add("Friday");
+		return days;
+	}
+	
+	public static ArrayList timeList() {
+		times.add("08:15 - 09:00");
+		times.add("09:15 - 10:00");
+		times.add("10:15 - 11:00");
+		times.add("11:15 - 12:00");
+		times.add("12:15 - 13:00");
+		times.add("13:15 - 14:00");
+		times.add("14:15 - 15:00");
+		times.add("15:15 - 16:00");
+		times.add("16:15 - 17:00");
+		times.add("17:15 - 18:00");
+		times.add("18:15 - 19:00");
+		times.add("19:15 - 20:00");
+		return times;
+	}
+	
+	public static void createTimetable() {
+		ArrayList time = timeList();
+		ArrayList days = daysList();
+		
+		for (Object day : days) {
+			for (Object t : time) {
+				System.out.println(day + " : " + t);
+			}
+		}
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//createTimetable();
+		System.out.println("Hello World");
 	}
-
 }
