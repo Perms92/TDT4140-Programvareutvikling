da8e0ae5b91cec3799a9e17e517acd861e5246f3
DjCreztian <creztian@hotmail.com>
2017-04-03 15:20:22 +0200

    updated GUI with 3 buttons to different subpages, working on logic next

--- src/GUI/MainScreen.java
+++ src/GUI/MainScreen.java
@@ -27,19 +27,20 @@ public class MainScreen extends Application{
 
 	}
 
-
+	//this runs when program starts
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		primaryStage.setTitle("RooME");
 
-		Scene scene = new Scene(loadScreenOne(), 900, 500); //, Color.rgb(227,201,176));
+		Scene scene = new Scene(loadScreenOne(), 900, 500);
 		scene.getStylesheets().add("file:stylesheet.css");
 		
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 
-		
+	
+	//first page in program, our menu
 	public GridPane loadScreenOne() {
 		//start grid
 		GridPane grid = new GridPane();
@@ -89,7 +90,8 @@ public class MainScreen extends Application{
 		editButton.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent e) {
-						
+				//going into the edit request view
+				editButton.getScene().setRoot(loadScreenFour());
 						
 			}
 		});
@@ -105,7 +107,7 @@ public class MainScreen extends Application{
 		return grid;
 	}
 	
-	
+	//this page is the book room page
 	public GridPane loadScreenTwo() {
         
 		GridPane grid = new GridPane();
@@ -115,33 +117,37 @@ public class MainScreen extends Application{
 		grid.setPadding(new Insets(30, 30, 30, 30));
 
 		//add labels
-		Text fag = new Text("Hvilket fag underviser du i?");
+		Text name = new Text("What is your name?");
+		name.getStyleClass().add("description");
+		grid.add(name, 0, 0);
+				
+		Text fag = new Text("What do you need?");
 		fag.getStyleClass().add("description");
-		grid.add(fag, 0, 0);
+		grid.add(fag, 0, 1);
 
-		Text projector = new Text("Trenger du projector?");
+		Text projector = new Text("Do you need a projector?");
 		projector.getStyleClass().add("description");
-		grid.add(projector, 0, 1);
+		grid.add(projector, 0, 2);
 
-		Text whiteboard = new Text("Trenger du blackboard?");
+		Text whiteboard = new Text("Do you need a blackboard?");
 		whiteboard.getStyleClass().add("description");
-		grid.add(whiteboard, 0, 2);
-
-		Text blackboard = new Text("Trenger du whiteboard?");
+		grid.add(whiteboard, 0, 3);
+				
+		Text blackboard = new Text("Do you need a whiteboard?");
 		blackboard.getStyleClass().add("description");
-		grid.add(blackboard, 0, 3);
+		grid.add(blackboard, 0, 4);
 
-		Text experimentable = new Text("Trenger du å gjøre eksperiment?");
+		Text experimentable = new Text("TBA?");
 		experimentable.getStyleClass().add("description");
-		grid.add(experimentable, 0, 4);
-		
-		Text studentCount = new Text("Hvor mange elever har du?");
+		grid.add(experimentable, 0, 5);
+				
+		Text studentCount = new Text("How many students do you need space for?");
 		studentCount.getStyleClass().add("description");
-		grid.add(studentCount, 0, 5);
+		grid.add(studentCount, 0, 6);
 
 		Text ny2 = new Text("TBA");
 		ny2.getStyleClass().add("description");
-		grid.add(ny2, 0, 6);
+		grid.add(ny2, 0, 7);
 
 
 		//add labels
@@ -173,10 +179,13 @@ public class MainScreen extends Application{
 		CheckBox cb4 = new CheckBox();
 		grid.add(cb4,1,4);
 		
+		CheckBox cb5 = new CheckBox();
+		grid.add(cb5,1,5);
+		
 		TextField amount = new TextField();
 		amount.setMaxSize(200.0, 30.0);
 		amount.setMinSize(200.0, 30.0);
-		grid.add(amount, 1, 5);
+		grid.add(amount, 1, 6);
 
 		//make all unchecked at start
 		cb1.setSelected(false);
@@ -255,8 +264,142 @@ public class MainScreen extends Application{
 		return grid;
     }
 	
-	
+	//this page is the page to save criteria
 	public GridPane loadScreenThree() {
+        
+		GridPane grid = new GridPane();
+		grid.setAlignment(Pos.CENTER);
+		grid.setHgap(50);
+		grid.setVgap(10);
+		grid.setPadding(new Insets(30, 30, 30, 30));
+
+		//add labels
+		Text name = new Text("What is your name?");
+		name.getStyleClass().add("description");
+		grid.add(name, 0, 0);
+				
+		Text fag = new Text("Which subject are you teaching?");
+		fag.getStyleClass().add("description");
+		grid.add(fag, 0, 1);
+
+		Text projector = new Text("Do you need a projector?");
+		projector.getStyleClass().add("description");
+		grid.add(projector, 0, 2);
+
+		Text whiteboard = new Text("Do you need a blackboard?");
+		whiteboard.getStyleClass().add("description");
+		grid.add(whiteboard, 0, 3);
+				
+		Text blackboard = new Text("Do you need a whiteboard?");
+		blackboard.getStyleClass().add("description");
+		grid.add(blackboard, 0, 4);
+
+		Text experimentable = new Text("TBA?");
+		experimentable.getStyleClass().add("description");
+		grid.add(experimentable, 0, 5);
+				
+		Text studentCount = new Text("How many students do you need space for?");
+		studentCount.getStyleClass().add("description");
+		grid.add(studentCount, 0, 6);
+
+		Text ny2 = new Text("TBA");
+		ny2.getStyleClass().add("description");
+		grid.add(ny2, 0, 7);
+
+
+		//add labels
+		Text warnings = new Text("");
+		fag.getStyleClass().add("description");
+		grid.add(warnings, 1, 8);
+				
+		//add labels
+		Text resultRooms = new Text("");
+		fag.getStyleClass().add("description");
+		grid.add(resultRooms, 1, 9);
+
+		//add inputs
+		TextField nameField = new TextField();
+		nameField.setMaxSize(200.0, 30.0);
+		nameField.setMinSize(200.0, 30.0);
+		grid.add(nameField,1,0);
+				
+		TextField subjectField = new TextField();
+		subjectField.setMaxSize(200.0, 30.0);
+		subjectField.setMinSize(200.0, 30.0);
+		grid.add(subjectField,1,1);
+				
+		//A checkbox without a caption
+		CheckBox cb1 = new CheckBox();
+		grid.add(cb1,1,2);
+				
+		CheckBox cb2 = new CheckBox();
+		grid.add(cb2,1,3);
+		
+		CheckBox cb3 = new CheckBox();
+		grid.add(cb3,1,4);
+				
+		CheckBox cb4 = new CheckBox();
+		grid.add(cb4,1,5);
+				
+		TextField amount = new TextField();
+		amount.setMaxSize(200.0, 30.0);
+		amount.setMinSize(200.0, 30.0);
+		grid.add(amount, 1, 6);
+
+		//make all unchecked at start
+		cb1.setSelected(false);
+		cb2.setSelected(false);
+		cb3.setSelected(false);
+		cb4.setSelected(false);
+
+		ListOfCriteria criteriaList = new ListOfCriteria();
+				
+		//add search button that triggers function when clicked
+		Button saveButton = new Button("Save your criteria");
+		saveButton.getStyleClass().add("button");
+		grid.add(saveButton, 1, 7);
+				
+		saveButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+			//feilhåndtering før funksjoner
+				String warningText = "";
+						
+				warnings.setText(warningText);
+						
+				if ((amount.getText()).equals("")) {
+					amount.setText("0");
+						
+					warningText = warningText + "You have to select how many students you need space for\n";
+					warnings.setText(warningText);
+				}
+									
+			
+				RoomCriteria crit = new RoomCriteria(Integer.parseInt((amount.getText())), Controller.checkValue(cb1.isSelected()), Controller.checkValue(cb2.isSelected()), Controller.checkValue(cb3.isSelected()), Controller.checkValue(cb2.isSelected()));
+				criteriaList.addCriteria(crit);
+						
+				//iterate through all criterias}
+				System.out.println(criteriaList.getCriteria(0));
+						
+			}
+		});
+
+		
+		Button backButton = new Button("Go back");
+		backButton.getStyleClass().add("button");
+		grid.add(backButton, 0, 7);
+        backButton.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent arg0) {
+                backButton.getScene().setRoot(loadScreenOne());             
+            }
+        });
+		return grid;
+    }
+	
+	//this page is the edit a previous request //currently not in use
+	public GridPane loadScreenFour() {
         
 		GridPane grid = new GridPane();
 		grid.setAlignment(Pos.CENTER);
