62927d0e413bc497c6c74ee625863729d650d83c
Trym Haakon Blaauw <thblaauw@stud.ntnu.no>
2017-04-10 13:43:04 +0200

    Made owner-attributtes in timetable-class as well as a verification method that ensures no timetable has both a person and a room as owner. Also wrote some SQL in our database to ensure that the 'owner'-value references an instance in the room-table. Hopefully will get to do the same with a person after a while

--- src/RooMe/Timetable.java
+++ src/RooMe/Timetable.java
@@ -1,13 +1,51 @@
 package RooMe;
+import java.sql.*;
+import Database.Database;
 
 public class Timetable {
 
+	public Person Person;
+	public Room Room;
 	
+	public Timetable() {
+		setPerson(null);
+		setRoom(null);
+	}
+	
+	
+	public Person getPerson() {
+		return Person;
+	}
+	public void setPerson(Person Person) {
+		this.Person = Person;
+	}
+	public Room getScheme() {
+		return Room;
+	}
+	public void setRoom(Room Room) {
+		this.Room = Room;
+	}
+
+
+	public void checkSingularity() throws IllegalStateException {
+		if (Person!=null && Room!=null)
+			throw new IllegalStateException("The timetable is either for a Person or a Room, not both!");
+	}
+	
+
 	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Timetable testtabell = new Timetable();
+		Person ække = new Person();
+		testtabell.setPerson(ække);
+		System.out.println("greit nok");
+		testtabell.checkSingularity();
+		Room Soverom = new Room("Test1", 100, true, false, true);
+		testtabell.setRoom(Soverom);
+		testtabell.checkSingularity();
+		System.out.println("kommer dette?");
 	}
 
 }
